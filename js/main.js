bad5b87f8c830defd35945aaaee36e9405086f39
Lazar Gumarov <gumarov.lazar@gmail.com>
2021-04-09 14:18:06 +0500

    part 1:26:55

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function(){
-
+    $('.header-slider').slick({
+        arrows:false,
+        dots:true,
+        vertical:true,
+        verticalSwiping: true,
+        dotsClass:'header-dots',
+      });
 });
+
+
+
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//       setting-name: setting-value
+//     });
+//   });
